e5e060deadd3ef21a4a927d64a10da71b9087c89
orpheydelorenzo739 <orpheydelorenzo739@gmail.com>
2025-03-14 09:53:24 +0000

    Add 8.py

---
+++ src/8.py
@@ -0,0 +1,7 @@
+def solve_math_problem(problem):
+    # Your code here
+    return result
+
+# Test your function with this problem:
+problem = "Find the value of x in the equation 2x + 5 = 11"
+print(solve_math_problem(problem))
