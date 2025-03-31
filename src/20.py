f1131c7e422ddda5ac3e3126e5f735b1dfb31d55
orpheydelorenzo739 <orpheydelorenzo739@gmail.com>
2025-03-31 00:38:49 +0000

    Add 20.py

---
+++ src/20.py
@@ -0,0 +1,7 @@
+def calculate_area(radius):
+    area = 3.14 * radius ** 2
+    return area
+
+radius = 5
+area = calculate_area(radius)
+print("The area of the circle with radius", radius, "is:", area)
