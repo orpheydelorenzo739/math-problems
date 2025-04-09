dccd6bd41bf7cb873f5c06041dce8d56d42d9999
orpheydelorenzo739 <orpheydelorenzo739@gmail.com>
2025-04-09 00:03:23 +0000

    Add 29.py

---
+++ src/29.py
@@ -0,0 +1,11 @@
+def add_numbers(a, b):
+    return a + b
+
+addition_result = add_numbers(5, 3)
+print(addition_result)
+
+def subtract_numbers(a, b):
+    return a - b
+
+subtraction_result = subtract_numbers(7, 2)
+print(subtraction_result)
